1090748c8eee78f82789115c3d64b9af88218426
Quang <143367844+SomaCruz0@users.noreply.github.com>
2025-05-14 14:54:26 +0700

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { saveUser } from "../utils/jwtUtils";
 
-const API_URL = "https://cte-quiz-ml27.onrender.com";
+const API_URL = "https://cte-quiz-ml27.onrender.com/api";
 
 // Create axios instance
 const api = axios.create({
